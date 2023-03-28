e68353f08812db6f3a3788d396f367d8edfbeb30
TimebotsGithubno2 <githubtimebots52752@gmail.com>
2023-03-28 18:34:46 -0500

    cleaned up Drivetrain.java

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import com.ctre.phoenix.sensors.Pigeon2;
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.ctre.phoenix.sensors.PigeonIMUConfiguration;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -45,10 +44,6 @@ public class Drivetrain extends SubsystemBase {
             DriveConstants.RIGHT_REAR_STEER_MOTOR_ID, DriveConstants.RIGHT_REAR_STEER_ENCODER_ID);
     private final SwerveModule leftRearSwerveModule = new SwerveModule(DriveConstants.LEFT_REAR_DRIVE_MOTOR_ID,
             DriveConstants.LEFT_REAR_STEER_MOTOR_ID, DriveConstants.LEFT_REAR_STEER_ENCODER_ID);
-
-    // private final AnalogGyro m_gyro = new AnalogGyro(0);
-
-    //public final GyroWrapperADIS16470_IMU imuADIS16470 = new GyroWrapperADIS16470_IMU();
 
     PigeonIMU gyroPigeonIMU = new PigeonIMU(Constants.DriveConstants.PIGEON_IMU_ID);
     Pigeon2 gyroPigeon2 = new Pigeon2(Constants.DriveConstants.PIGEON_2_ID);
@@ -93,14 +88,7 @@ public class Drivetrain extends SubsystemBase {
         SmartDashboard.putString("odometry getRotation",
                 m_odometry.getPoseMeters().getRotation().toString());
 
-        // SmartDashboard.putNumber("LeftFrontSpeed",
-        // swerveModuleStates[0].speedMetersPerSecond );
-        // SmartDashboard.putNumber("LeftFrontAngle",
-        // swerveModuleStates[0].angle.getDegrees() );
-
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.MAX_DRIVE_SPEED);
-        // SmartDashboard.putNumber("LeftFrontSpeedNorm",
-        // swerveModuleStates[0].speedMetersPerSecond );
 
         leftFrontSwerveModule.setDesiredState(swerveModuleStates[0], true, "LF");
         rightFrontSwerveModule.setDesiredState(swerveModuleStates[1], true, "RF");
@@ -108,82 +96,37 @@ public class Drivetrain extends SubsystemBase {
         leftRearSwerveModule.setDesiredState(swerveModuleStates[3], true, "LR");
     }
 
-    /**
-     * Returns the currently-estimated pose of the robot.
-     *
-     * @return The pose.
-     */
+    /* Returns the currently-estimated pose of the robot.*/
     public Pose2d getPose() {
         return m_odometry.getPoseMeters();
     }
 
-    /** Updates the field relative position of the robot. */
+    /* Updates the field relative position of the robot. */
     public void updateOdometry() {
         m_odometry.update(this.getHeading(), modulePositions);
 
     }
 
-    /**
-     * Resets the odometry Position and Angle to 0.
-     */
+    /* Resets the odometry Position and Angle to 0. */
     public void resetOdometry() {
         System.out.println("resetOdometry");
         m_odometry.resetPosition(this.getHeading(), modulePositions, new Pose2d());
     }
 
-    /**
-     * Resets the odometry to the specified pose.
-     *
-     * @param pose The pose to which to set the odometry.
-     */
+    /* Resets the odometry to the specified pose. */
     public void resetOdometryWithPose2d(Pose2d pose) {
         System.out.println("resetOdometryWithPose2d");
-        m_odometry.resetPosition(pose.getRotation(), modulePositions, pose); // imuADIS16470.getRotation2d()
-    }
-
-    /** Zeroes the heading of the robot. */
-    // public void resetADIS16470() {
-    // System.out.println("resetADIS16470");
-    // imuADIS16470.reset();
-    // }
-
-    /** calibrate the heading of the robot. */
-    // public void calibrateADIS16470() {
-    // System.out.println("calibrateADIS16470");
-    // imuADIS16470.calibrate();
-    // }
-
-    public void calibratePigeonIMU() {
-
+        m_odometry.resetPosition(pose.getRotation(), modulePositions, pose);
     }
 
     public void resetPIgeonIMU() {
         gyroPigeon2.setYaw(0);
     }
 
-    /**
-     * Returns the heading of the robot.
-     * 
-     * @return the robot's heading in degrees, from -180 to 180. // ! This comment
-     *         was from last year.
-     */
-    public Rotation2d getHeading() {
+    /* @return the robot's heading in degrees, from -180 to 180. */
+    public Rotation2d getHeading() { return Rotation2d.fromDegrees(gyroPigeon2.getYaw()); }
 
-        Rotation2d heading = Rotation2d.fromDegrees(gyroPigeon2.getYaw());
-
-        // System.out.println( "getYComplementaryAngle = " + heading );
-        // System.out.println( "getXComplementaryAngle = " +
-        // imuADIS16470.getXComplementaryAngle() );
-
-        return heading; // TODO Lucas //.minus(new Rotation2d(this.autoTurnOffsetRadians)); // radians
-
-    }
-
-    /**
-     * Sets the swerve ModuleStates.
-     * 
-     * @param desiredStates The desired SwerveModule states.
-     */
+    /* @param desiredStates The desired SwerveModule states. */
     public void setModuleStates(SwerveModuleState[] desiredStates) {
         SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.MAX_DRIVE_SPEED);
 
@@ -206,84 +149,4 @@ public class Drivetrain extends SubsystemBase {
         Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(list, config);
         return exampleTrajectory;
     }
-
-    /**
-     * ! We might need this for the Auto. -Lucas
-     * 
-     * // TODO: Find out if we still need this. -Lucas
-     * // private double autoTurnOffsetRadians = 0;
-     * 
-     * 
-     * public void setAutoTurnOffsetRadians(double angleInRadians) { // TODO -Lucas
-     * System.out.println("angleInRadians = " + angleInRadians);
-     * this.autoTurnOffsetRadians = angleInRadians;
-     * }
-     * 
-     * 
-     * protected static Trajectory loadTrajectory(String trajectoryName) throws
-     * IOException {
-     * return TrajectoryUtil.fromPathweaverJson(
-     * Filesystem.getDeployDirectory().toPath().resolve(Paths.get("output",
-     * trajectoryName + ".wpilib.json")));
-     * }
-     * 
-     * public Trajectory loadTrajectoryFromFile(String filename) {
-     * try {
-     * return loadTrajectory(filename);
-     * } catch (IOException e) {
-     * DriverStation.reportError("Failed to load auto trajectory: " + filename,
-     * false);
-     * return new Trajectory();
-     * }
-     * }
-     * 
-     * 
-     * 
-     * public static Trajectory generateTrajectory(TrajectoryConfig config, Pose2d
-     * startingPose2d,
-     * List<Translation2d> list) {
-     * // Pose2d offset = new Pose2d(startingPose2d.getX(), startingPose2d.getY(),
-     * // startingPose2d.getRotation());
-     * 
-     * ArrayList<Translation2d> newList = new ArrayList<Translation2d>();
-     * 
-     * for (int i = 0; i < list.size(); i++) {
-     * // double x = (list.get(i).getX() - startingPose2d.getX()) * 30 * .0254;
-     * // double y = (list.get(i).getY() - startingPose2d.getY()) * 30 * .0254;
-     * 
-     * double x = list.get(i).getX() * 30 * .0254;
-     * double y = list.get(i).getY() * 30 * .0254;
-     * newList.add(new Translation2d(x, y));
-     * System.out.println("x = " + x + " y = " + y);
-     * }
-     * 
-     * Translation2d end2d = newList.remove(newList.size() - 1);
-     * 
-     * Pose2d end = new Pose2d(end2d.getX(), end2d.getY(),
-     * startingPose2d.getRotation());
-     * 
-     * return TrajectoryGenerator.generateTrajectory(new
-     * Pose2d(startingPose2d.getX() * 30 * .0254,
-     * startingPose2d.getY() * 30 * .0254, startingPose2d.getRotation()), list, end,
-     * config);
-     * 
-     * // Pose2d offset = new Pose2d(startingPose2d.getX(), startingPose2d.getY(),
-     * // startingPose2d.getRotation());
-     * // ArrayList<Pose2d> newList = new ArrayList<Pose2d>();
-     * // newList.add(list.get(0));
-     * 
-     * // for (int i = 1; i < list.size(); i++) {
-     * // double x = (list.get(i).getX() - offset.getX()) * 30 * .0254;
-     * // double y = (list.get(i).getY() - offset.getY()) * 30 * .0254;
-     * // newList.add(new Pose2d(x, y, list.get(i).getRotation()));
-     * // System.out.println("x = " + x + " y = " + y);
-     * // }
-     * 
-     * // Trajectory exampleTrajeactory =
-     * // TrajectoryGenerator.generateTrajectory(newList, config);
-     * 
-     * // return exampleTrajeactory;
-     * }
-     */
-
 }
